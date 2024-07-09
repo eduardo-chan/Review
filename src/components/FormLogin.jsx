ba3789328bb1bb137b85de4481eec704e4b0b3fc
eduardo-chan <20213tn001@utez.edu.mx>
2024-07-09 11:59:04 -0600

    [UPDATE] FormLogin

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,61 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
-import { Input, Image, Icon } from '@rneui/base';
-import { set } from "lodash";
+import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Image, Input, Icon } from "@rneui/base";
+import { isEmpty } from "lodash";
 
 export default function FormLogin() {
-  const [username, setUsername] = useState(null)
-  const [password, setPassword] = useState(null)
-  const [showPassword, setShowPassword] = useState(true)
-  const [error, setError] = useState({username: '', passwordError: ''})
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [showPassword, setShowPassword] = useState(true);
+  const [error, seterror] = useState({usernameError: "", passwordError: ""})
 
-  console.log("ya entro?")
-  console.log("username: ", username)
-  console.log("password: ", password)
-
-  if (isEmpty(username) || isEmpty(password)) {
-    setError({usernameError: 'el usuario es requerido', passwordError: 'la contraseña es requerida'})
-  } else {
-    setError({usernameError: '', passwordError: ''})
-  }
+  const login = () => {
+    console.log("Username: ", username);
+    console.log("Password: ", password);
+    if(isEmpty(username) || isEmpty(password)){
+      seterror({usernameError: "Requerido", passwordError: "Requerido"})
+    } else {
+      seterror({usernameError: "", passwordError: ""})
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Image
         source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/carsishop-60e8f.appspot.com/o/images-products%2F33ef818f-c678-46fc-aa92-c70478d4991a.jpg?alt=media&token=14aa6958-f6ee-4aaf-921e-4df4156ad1f0' }}
-        style={{width: 100, height: 100, marginBottom: 16}}
+        style={{ height: 100, width: 100, marginVertical: 20 }}
       />
       <Input
-        placeholder='User'
-        label='User'
+        placeholder="Usuario"
+        label="Usuario"
         labelStyle={styles.label}
-        style={{marginBottom: 16 }}
+        containerStyle={{ marginBottom: 16 }}
+        onChangeText={(username) => setUsername(username)}
         errorMessage={error.usernameError}
       />
-      <input
-        placeholder='********'
-        label='Password'
+      <Input
+        placeholder="********"
+        label="Contraseña"
         labelStyle={styles.label}
-        style={{marginBottom: 16 }}
-        secureTextEntry={true}
+        containerStyle={{ marginBottom: 16 }}
+        secureTextEntry={showPassword}
         rightIcon={
           <Icon
             name={showPassword ? "eye-off" : "eye"}
             type="material-community"
             onPress={() => setShowPassword(!showPassword)}
-          />  
+          />
         }
-        onChangeText={event => setPassword(event)}
+        onChangeText={(password) => setPassword(password)}
         errorMessage={error.passwordError}
       />
       <Button
-        tittle={"Login"}
-        containerStyle={{width: "80%", borderColor: 'green', borderWidth: 1, borderRadius: 16 }}
-        color={"white"}
-        tittleStyle={{ color: "green "}}
-        icon={<Icon name="login" type="material-community" color="green" />}
-        iconPosition="left"
         onPress={login}
+        title={"Iniciar sesión"}
+        titleStyle={{ color: "pink" }}
+        containerStyle={{
+          width: "80%",
+          borderColor: "pink",
+          borderWidth: 1,
+          borderRadius: 16,
+        }}
+        color={"white"}
+        iconPosition="left"
+        icon={
+          <Icon
+            name="login"
+            type="material-community"
+            color={"pink"}
+            style={{ marginRight: 5 }}
+          />
+        }
       />
     </View>
   );
@@ -63,11 +76,11 @@ export default function FormLogin() {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   label: {
-    fontSize: 12,
-    color: 'green',
+    fontSize: 15,
+    color: "#34fa",
   },
 });
